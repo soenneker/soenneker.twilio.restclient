b963a96043d10eb38b8bd43ae7d2dc7a1c326c0a
Jake Soenneker <jake@soenneker.com>
2025-02-05 14:47:13 -0600

    Build

--- src/Registrars/TwilioRestClientUtilRegistrar.cs
+++ src/Registrars/TwilioRestClientUtilRegistrar.cs
@@ -13,18 +13,22 @@ public static class TwilioRestClientUtilRegistrar
     /// <summary>
     /// Adds <see cref="ITwilioRestClientUtil"/> as a singleton service. <para/>
     /// </summary>
-    public static void AddTwilioRestClientUtilAsSingleton(this IServiceCollection services)
+    public static IServiceCollection AddTwilioRestClientUtilAsSingleton(this IServiceCollection services)
     {
-        services.AddHttpClientCache();
-        services.TryAddSingleton<ITwilioRestClientUtil, TwilioRestClientUtil>();
+        services.AddHttpClientCacheAsSingleton()
+                .TryAddSingleton<ITwilioRestClientUtil, TwilioRestClientUtil>();
+
+        return services;
     }
 
     /// <summary>
     /// Adds <see cref="ITwilioRestClientUtil"/> as a scoped service. <para/>
     /// </summary>
-    public static void AddTwilioRestClientUtilAsScoped(this IServiceCollection services)
+    public static IServiceCollection AddTwilioRestClientUtilAsScoped(this IServiceCollection services)
     {
-        services.AddHttpClientCache();
-        services.TryAddScoped<ITwilioRestClientUtil, TwilioRestClientUtil>();
+        services.AddHttpClientCacheAsSingleton()
+                .TryAddScoped<ITwilioRestClientUtil, TwilioRestClientUtil>();
+
+        return services;
     }
 }
